308993d28b38809ed0b41f902e9e573d00a99227
Ed Charbeneau <ed.charbeneau@gmail.com>
2013-01-30 19:57:33 -0500

    Configured to use MVC conventions
    Removed Site.css
    Changed compiled css directory from 'stylesheets' to Content using the
    config.rb file
    Recompiled the scss files
    Changed the bundle config to 'screen.css' from 'site.css'

--- SimpleCompassMVCExample/App_Start/BundleConfig.cs
+++ SimpleCompassMVCExample/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace SimpleCompassMVCExample
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/screen.css"));
 
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
                         "~/Content/themes/base/jquery.ui.core.css",
